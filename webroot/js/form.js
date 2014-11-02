a5f48fe1848a1f43b6a98c5e753040b3b881e7c2
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-11-02 00:43:21 -0300

    Se permite chequear un correo (con check: email) o varios (con check: emails)

--- webroot/js/form.js
+++ webroot/js/form.js
@@ -71,6 +71,21 @@ Form.check_email = function (field) {
     'use strict';
     var filter, emails, i;
     filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(field.value)) {
+        return "¡%s no es válido!";
+    }
+    return true;
+};
+
+/**
+ * Método que revisa que el campo sea uno o varios correos electrónicos
+ * @param Campo que se quiere validar
+ * @return =true pasó la validación ok
+ */
+Form.check_emails = function (field) {
+    'use strict';
+    var filter, emails, i;
+    filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     emails = field.value.replace(',', ';').replace(' ', '').split(';');
     for (i = 0; i < emails.length; i=i+1) {
         if (!filter.test(emails[i])) {

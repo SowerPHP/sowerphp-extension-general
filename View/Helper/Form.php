5efa16361a17f396fc70faa550e0636160f82cfe
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-11-22 14:00:58 -0300

    Se detecta de forma correcta el valor por defecto de un checkbox en Form::_js()

--- View/Helper/Form.php
+++ View/Helper/Form.php
@@ -393,7 +393,10 @@ class View_Helper_Form
             foreach ($config['values'] as $value) {
                 $values .= '<tr>';
                 foreach ($config['inputs'] as $input) {
-                    $input['value'] = $value[$input['name']];
+                    if (isset($input['type']) && $input['type']=='checkbox')
+                        $input['checked'] = $value[$input['name']];
+                    else
+                        $input['value'] = $value[$input['name']];
                     $input['name'] = $input['name'].'[]';
                     $d = (isset($input['type']) && $input['type']=='hidden') ? ' style="display:none;"' : '';
                     $values .= '<td'.$d.'>'.rtrim($this->input($input)).'</td>';

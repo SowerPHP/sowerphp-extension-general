573c3eeca10acd3be05d81b58d6fd1cf24a84114
Esteban De La Fuente Rubio <esteban@delaf.cl>
2019-07-23 12:08:14 -0400

    Se agrega recaptcha a formulario de contacto si está disponible

--- Controller/Contacto.php
+++ Controller/Contacto.php
@@ -38,15 +38,16 @@ class Controller_Contacto extends \Controller_App
      */
     public function beforeFilter()
     {
-        if (isset($this->Auth))
+        if (isset($this->Auth)) {
             $this->Auth->allow('index');
+        }
         parent::beforeFilter();
     }
 
     /**
      * Método que desplegará y procesará el formulario de contacto
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2018-10-15
+     * @version 2019-07-23
      */
     public function index()
     {
@@ -54,12 +55,41 @@ class Controller_Contacto extends \Controller_App
         // permirir cargar página de contacto
         if (\sowerphp\core\Configure::read('email.default')===NULL) {
             \sowerphp\core\Model_Datasource_Session::message(
-                'Página de contacto deshabilitada', 'error'
+                __('La página de contacto no se encuentra disponible'), 'error'
             );
             $this->redirect('/');
         }
+        // colocar variable para captcha (si está configurado)
+        $captcha_public_key = \sowerphp\core\Configure::read('recaptcha.public_key');
+        if ($captcha_public_key) {
+            $this->set([
+                'captcha_public_key' => $captcha_public_key,
+                'language' => \sowerphp\core\Configure::read('language'),
+            ]);
+        }
         // si se envió el formulario se procesa
         if (isset($_POST['submit'])) {
+            // si existe la configuración para recaptcha se debe validar
+            $captcha_private_key = \sowerphp\core\Configure::read('recaptcha.private_key');
+            if ($captcha_private_key) {
+                if (empty($_POST['g-recaptcha-response'])) {
+                    \sowerphp\core\Model_Datasource_Session::message(
+                        __('Debe completar el captcha para enviar su mensaje'), 'warning'
+                    );
+                    $this->response->status(412);
+                    return;
+                }
+                $recaptcha = new \ReCaptcha\ReCaptcha($captcha_private_key);
+                $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+                if (!$resp->isSuccess()) {
+                    \sowerphp\core\Model_Datasource_Session::message(
+                        __('El captcha enviado es incorrecto'), 'error'
+                    );
+                    $this->response->status(412);
+                    return;
+                }
+            }
+            // enviar email
             $_POST['nombre'] = trim(strip_tags($_POST['nombre']));
             $_POST['correo'] = trim(strip_tags($_POST['correo']));
             $_POST['mensaje'] = trim(strip_tags($_POST['mensaje']));
@@ -67,22 +97,22 @@ class Controller_Contacto extends \Controller_App
                 $email = new \sowerphp\core\Network_Email();
                 $email->replyTo($_POST['correo'], $_POST['nombre']);
                 $email->to(\sowerphp\core\Configure::read('email.default.to'));
-                $email->subject('Contacto desde la web #'.date('YmdHis'));
+                $email->subject('Contacto desde '.$this->request->url.' #'.date('YmdHis'));
                 $msg = $_POST['mensaje']."\n\n".'-- '."\n".$_POST['nombre']."\n".$_POST['correo'];
                 $status = $email->send($msg);
                 if ($status===true) {
                     \sowerphp\core\Model_Datasource_Session::message(
-                        'Su mensaje ha sido enviado, se responderá a la brevedad.', 'ok'
+                        __('Su mensaje ha sido enviado, se responderá a la brevedad.'), 'ok'
                     );
                     $this->redirect('/contacto');
                 } else {
                     \sowerphp\core\Model_Datasource_Session::message(
-                        'Ha ocurrido un error al intentar enviar su mensaje, por favor intente nuevamente.<br /><em>'.$status['message'].'</em>', 'error'
+                        __('Ha ocurrido un error al enviar su mensaje, por favor intente nuevamente.<br /><em>%s</em>', $status['message']), 'error'
                     );
                 }
             } else {
                 \sowerphp\core\Model_Datasource_Session::message(
-                    'Debe completar todos los campos del formulario', 'error'
+                    __('Por favor, completar todos los campos del formulario'), 'error'
                 );
             }
         }
